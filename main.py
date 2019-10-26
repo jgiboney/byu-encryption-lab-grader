8419f42454b68b8a1b6a6a10922328f62752565f
Justin Giboney <jgiboney@gmail.com>
2019-10-25 18:10:17 -0600

    adding more tests

--- main.py
+++ main.py
@@ -15,8 +15,9 @@
 
 import importlib, os
 
-# Setup the student libraries to run
-studentModuleDirectory = 'studentLibrary/' # This should be a command line parameter
+
+# Setup the student libraries to run.
+studentModuleDirectory = 'studentLibrary/' # This should be in a config file
 studentModules = {}
 
 for module in os.listdir(os.path.dirname(studentModuleDirectory)):
@@ -25,5 +26,82 @@ for module in os.listdir(os.path.dirname(studentModuleDirectory)):
         studentModules[module] = importlib.import_module(module_path[:-3])
 del module
 
+
+# These should be in a config file
+key = 'attacker'
+message = 'thefighterswillstriketheenemybasesatnoona5'
+transposeColumnarEncryptedMessage = 'teteeafwkmt iieyn hlhao gltbo tsesn hrrns5esiea '
+transposeColumnarDecryptedMessage = 'thefighterswillstriketheenemybasesatnoona5      '
+alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
+rotation = 5
+substitutionRotationEncryptedMessage = 'ymjknlmyjwx1nqqxywnpjymjjsjr3gfxjxfysttsfa'
+runningKeyAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+runningKeyMessage = 'richardwill'
+runningKeyKey = 'withmuchint'
+runningKeyEncryptedMessage = 'nqvomlfdqye'
+
+
+# Loop through the student libraries and run the encryption and decryption functions.
 for module in studentModules.keys():
-    studentModules[module].sayHello()
+
+    try:
+        encryptedString = studentModules[module].substitutionRotationEncrypt(alphabet,rotation,message)
+        if encryptedString == substitutionRotationEncryptedMessage:
+            print('substitution rotation encryption correct.')
+        else:
+            print('substitution rotation encryption NOT correct.')
+    except:
+        print('running substitutionRotationEncrypt failed.')
+
+    try:
+        decryptedString = studentModules[module].substitutionRotationDecrypt(alphabet,rotation,substitutionRotationEncryptedMessage)
+        if decryptedString == message:
+            print('substitution rotation decryption correct.')
+        else:
+            print('substitution rotation decryption NOT correct.')
+    except:
+        print('running substitutionRotationDecrypt failed.')
+
+    try:
+        encryptedString = studentModules[module].runningKeyCipherEncrypt(runningKeyAlphabet,runningKeyKey,runningKeyMessage)
+        if encryptedString == runningKeyEncryptedMessage:
+            print('running key encryption correct.')
+        else:
+            print('running key encryption NOT correct.')
+    except:
+        print('running runningKeyEncryption failed.')
+
+    try:
+        decryptedString = studentModules[module].runningKeyCipherDecrypt(runningKeyAlphabet,runningKeyKey,runningKeyEncryptedMessage)
+        if decryptedString == runningKeyMessage:
+            print('running key decryption correct.')
+        else:
+            print('running key decryption NOT correct.')
+    except:
+        print('running runningKeyDecrypt failed.')
+
+    try:
+        encryptedString = studentModules[module].transposeColumnarEncrypt(key,message)
+        if encryptedString == transposeColumnarEncryptedMessage:
+            print('transpose columnar encryption correct.')
+        else:
+            print('transpose columnar encryption NOT correct.')
+    except:
+        print('running transposeColumnarEncrypt failed.')
+
+    try:
+        decryptedMessage = studentModules[module].transposeColumnarDecrypt(key,transposeColumnarEncryptedMessage)
+        if transposeColumnarDecryptedMessage == decryptedMessage:
+            print('transpose columnar decryption correct.')
+        else:
+            print('transpose columnar decryption NOT correct.')
+    except:
+        print('running transposeColumnarDecrypt failed.')
+
+
+    studentModules[module].desHexEncrypt('133457799BBCDFF1','0123456789ABCDEF')
+
+
+
+
+
